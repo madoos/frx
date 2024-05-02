5a5ca421735ae635aa66b5160b34cf0744b82d25
Maurice <maurice@MacBook-Air-de-Maurice-dominguez.local>
2024-05-02 21:13:46 +0200

    chore: adds empty observable creator tsdocs

--- src/internal/observable/empty.ts
+++ src/internal/observable/empty.ts
@@ -1,6 +1,9 @@
 import { observable } from '.';
 import { noop } from '../helpers';
 
+/**
+ * Creates an observable that completes immediately without emitting any values.
+ */
 export const empty = observable<never>((_next, _error, complete) => {
   complete();
   return noop;
