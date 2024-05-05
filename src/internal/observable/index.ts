e0e2ac0ae9742badb94e0d2913716b92bf5857a6
Maurice <maurice@MacBook-Air-de-Maurice-dominguez.local>
2024-05-05 14:20:37 +0200

    refactor: improve unsubscription ref to ensure observer protocol

--- src/internal/observable/index.ts
+++ src/internal/observable/index.ts
@@ -1,20 +1,25 @@
 import type { Producer, Observable, Unsubscribe } from '../types';
 import { observer } from '../observer';
-import { noop } from '../helpers';
+import { noop, tryCatch } from '../helpers';
 
 export const observable =
   <T>(producer: Producer<T>): Observable<T> =>
   (next = noop<T>, error = noop, complete = noop<void>) => {
     let unsubscribe: Unsubscribe = noop;
+    const ensureUnsubscribe = () => unsubscribe();
 
     const [next_, error_, _complete_] = observer(
       next,
       error,
       complete,
-      unsubscribe
+      ensureUnsubscribe
     );
 
-    unsubscribe = producer(next_, error_, _complete_);
+    unsubscribe = tryCatch(
+      () => producer(next_, error_, _complete_),
+      error,
+      noop
+    );
 
     return unsubscribe;
   };
